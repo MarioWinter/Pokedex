107e22591dcac11c93766743bd867cbce4414c44
Mario Müller <mariomuller@MacBook-Pro.fritz.box>
2023-09-24 10:48:06 +0200

    add blur backround and Pokemon Info card

--- script.js
+++ script.js
@@ -17,14 +17,18 @@ async function loadPokemon() {
     let responseAsJason = await response.json();
 
     document.getElementById('pokedex-container').innerHTML = `
-        <div>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="25" height="25">
-                <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 288 480 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-370.7 0 73.4-73.4c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-128 128z" fill="white" stroke="white" stroke-width="2"/>
-            </svg>
+        <div class="pokedex-content">
+            <div class="pokedex-header">
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="25" height="25">
+                        <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 288 480 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-370.7 0 73.4-73.4c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-128 128z" fill="white" stroke="white" stroke-width="2"/>
+                    </svg>
+                </div>
+                <div>
+                    <b>${responseAsJason['name']}</b>
+                </div>
+            </div>
+            <div id="pokemon-info"></div>
         </div>
-        <div>
-            <b>${responseAsJason['name']}</b>
-        </div>
-        
         `;
 }

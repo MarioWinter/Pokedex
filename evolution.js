ad410001fe118d0c8c30a37bc0391650c5f95db6
Mario Müller <mariomuller@macbook-pro.speedport.ip>
2023-10-26 23:15:23 +0200

    In the Pokemon evolutions changed the direction of the arrow

--- evolution.js
+++ evolution.js
@@ -59,9 +59,7 @@ function AddTwoEvolutions(PokeID1, PokeID2, j) {
         <div class="pokeEvolutionContainer">
             <img class="pokemon-img-pocket" src="${img1}" alt="Pokemon Evolution1">
 
-            <svg class="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="20" height="20" transform="rotate(180)">
-                    <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 288 480 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-370.7 0 73.4-73.4c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-128 128z" fill="black" stroke="black" stroke-width="2"/>
-            </svg>
+            <img width="20" height="20" src="./icon/arrow-right-long-solid-black.svg" alt="Right Arrow">
 
             <img class="pokemon-img-pocket" src="${img2}" alt="Pokemon Evolution1">
         </div>    
@@ -87,15 +85,11 @@ function AddThreeEvolutions(PokeID1, PokeID2, PokeID3, j) {
         <div class="pokeEvolutionContainer">
             <img class="pokemon-img-pocket2" src="${img1}" alt="Pokemon frist evolution">
 
-            <svg class="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="20" height="20" transform="rotate(180)">
-                    <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 288 480 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-370.7 0 73.4-73.4c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-128 128z" fill="black" stroke="black" stroke-width="2"/>
-            </svg>
+            <img width="20" height="20" src="./icon/arrow-right-long-solid-black.svg" alt="Reight Arrow">
 
             <img class="pokemon-img-pocket2" src="${img2}" alt="Pokemon second evolution">
 
-            <svg class="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="20" height="20" transform="rotate(180)">
-                    <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 288 480 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-370.7 0 73.4-73.4c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-128 128z" fill="black" stroke="black" stroke-width="2"/>
-            </svg>
+            <img width="20" height="20" src="./icon/arrow-right-long-solid-black.svg" alt="Reight Arrow">
 
             <img class="pokemon-img-pocket" src="${img3}" alt="Pokemon thrid evolution">
         </div>    
